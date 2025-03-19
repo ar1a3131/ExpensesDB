953e3a2b1b31a8aa5bae3eeba9e487e72976b16f
ar1a3131 <aziziarianna@gmail.com>
2025-03-19 13:17:38 -0500

    added photos

--- backend/update-backend.sh
+++ backend/update-backend.sh
@@ -2,29 +2,25 @@
 
 # Stop the backend container
 echo "Stopping the existing backend container..."
-sudo docker stop backend-container
+sudo docker stop backend-container-maintenance
 
 # Remove the backend container
 echo "Removing the existing backend container..."
-sudo docker rm backend-container
+sudo docker rm backend-container-maintenance
 
 # Build the updated backend image
 echo "Building the updated backend image..."
 sudo docker build -t backend-app .
 
-# Ensure the custom network exists
-sudo docker network inspect app-network >/dev/null 2>&1 || \
-    sudo docker network create app-network
-
 # Start the new backend container
 echo "Starting the new backend container..."
 sudo docker run -d \
-    -p 5002:5002 \
-    --name backend-container \
+    -p 5004:5004 \
+    --name backend-container-maintenance \
     --env-file /volume1/ExpensesDB/backend/.env \
     --network expenses-network \
     backend-app
 
 # Show logs to verify that the backend is running
 echo "Showing backend logs..."
-sudo docker logs -f backend-container
+sudo docker logs -f backend-container-maintenance
